a5e484be56a3b9fa6e9f2fed76aea2a02640f8c7
dacortes <dacortes@student.42barcelona.com>
2023-03-13 22:26:23 +0100

    add ex02 ok

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,21 +1,25 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    exec.py                                            :+:      :+:    :+:    #
+#    whois.py                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/03/13 21:57:53 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/13 21:58:01 by dacortes         ###   ########.fr        #
+#    Created: 2023/03/13 21:57:43 by dacortes          #+#    #+#              #
+#    Updated: 2023/03/13 22:24:13 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
 
-list = sys.argv[1:]
-if list:
-    tmp = ' '.join(list[::1])
-    swap = tmp.swapcase()
-    print(swap[::-1])
+if len(sys.argv) == 2 and isinstance(sys.argv[1], int):
+    num = sys.argv[1::]
+    n = int(num[0])
+    if n == 0 :
+        print("I'm Zero")
+    elif n % 2 == 0:
+        print("I'm Even")
+    else:
+        print("I'm Odd")
 else :
     exit

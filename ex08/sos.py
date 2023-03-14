6fb31fd19914dcbaf4f3618ba1390e4a3672f722
dacortes <dacortes@student.42barcelona.com>
2023-03-14 22:27:42 +0100

    ex08 ok

--- ex08/sos.py
+++ ex08/sos.py
@@ -6,7 +6,7 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/14 20:53:39 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/14 21:37:18 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/14 22:26:31 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,20 +23,36 @@ alphabet={
     "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
     "Y": "-.--", "Z": "--.."
 }
-if len(sys.argv) == 2:
-    arg = sys.argv[1::]
-    morse = arg[0]
-    res = []
+def check_error(morse):
     i = 0
-    print(morse)
     while i < len(morse):
-        if morse[i].isdigit() == True:
-            res.append(numbers[morse[i]])
-        elif morse[i].isalpha() == True:
-            res.append(alphabet[morse[i].upper()])
-        elif morse[i] == ' ':
-            res.append('/')
-        i += 1
-    print(*res)
-#"96 BOULEVARD" "Bessiere"
-#----. -.... / -... --- ..- .-.. . ...- .- .-. -.. / -... . ... ... .. . .-. .
+        if morse[i] == ' ' or morse[i].isdigit() == True or morse[i].isalpha() == True:
+            i += 1
+        else :
+            print("ERROR")
+            return (False)
+    return (True)
+def more_than_two(arg):
+    union = None
+    if len(arg) > 1:
+        union = " ".join(arg)
+        return (union)
+    union = arg[0]
+    return (union)
+if len(sys.argv) >= 2:
+    arg = sys.argv[1::]
+    morse = more_than_two(arg)
+    if check_error(morse) == True :
+        res = []
+        i = 0
+        while i < len(morse):
+            if morse[i].isdigit() == True:
+                res.append(numbers[morse[i]])
+            elif morse[i].isalpha() == True:
+                res.append(alphabet[morse[i].upper()])
+            elif morse[i] == ' ':
+                res.append('/')
+            i += 1
+        print(*res)
+    else :
+        exit

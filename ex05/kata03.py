92c788307655c2fe5a0c6e386c5137d8b9b5bc9a
dacortes <dacortes@student.42barcelona.com>
2023-03-14 12:18:42 +0100

    katas full OK

--- ex05/kata03.py
+++ ex05/kata03.py
@@ -6,9 +6,10 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/14 09:33:56 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/14 09:40:14 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/14 12:10:55 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # Put this at the top of your kata03.py file
 kata = "The right format"
+print(f"{kata:->42}",end = "")
--- ex05/kata04.py
+++ ex05/kata04.py
@@ -6,9 +6,11 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/14 09:34:01 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/14 09:40:37 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/14 12:18:20 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 # Put this at the top of your kata04.py file
 kata = (0, 4, 132.42222, 10000, 12345.67)
+result = "module_{:02d}, ex_{:02d} : {:.2f}, {:.2e}, {:.2e}".format(kata[0], kata[1], round(kata[2], 2), kata[3], kata[4])
+print(result)

0259e91463d1cc81f22bdb5be259908a91748017
dacortes <dacortes@student.42barcelona.com>
2023-03-14 17:59:52 +0100

    add recipe ok

--- ex06/recipe.py
+++ ex06/recipe.py
@@ -6,7 +6,7 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/14 12:21:39 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/14 16:40:37 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/14 17:58:57 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -44,12 +44,19 @@ def print_name(cookbook):
         str_i = f"{i}."
         print("  ",str_i,key)
 def print_details(cookbook,name):
-    print("Recipe for cake:")
-    print("   Ingredients list:",cookbook[name]['ingredients'])
-    print("   To be eaten for",*cookbook[name]['meal'])
-    print("   Takes",*cookbook[name]['prep_time'],"minutes of cooking.")
+    if name not in cookbook:
+        print("Sorry, this recipe does not exist.")
+    else:
+        print("\nRecipe for", name,end=":")
+        print("\n   Ingredients list:",cookbook[name]['ingredients'])
+        print("   To be eaten for",*cookbook[name]['meal'])
+        print("   Takes",*cookbook[name]['prep_time'],"minutes of cooking.")
 def delete_recipe(cookbook,name):
-    del cookbook[name]
+    print("Recipe to delete")
+    if name not in cookbook:
+        print("Sorry, this recipe does not exist.")
+    else :
+        del cookbook[name]
 def print_menu():
     print("List of available option:")
     print("      1: Add a recipe")
@@ -58,18 +65,31 @@ def print_menu():
     print("      4: Print the cookbook")
     print("      5: Quit")
     print("\nPlease select an option:")
-#add_recipe(cookbook, 'pastel', ['huevo', 'azucar', 'chocolate'], ['postre'], ['65'])
 print("Welcome to the Python Cookbook !")
 print_menu()
 quit = True
 while quit:
     opt = input()
     if opt == '1':
-        add_recipe(cookbook,input("Enter a name:\n"),input("Enter ingredients:\n"),input("Enter a meal type:\n"),input("Enter a preparation time:\n"))
+        ingredients =[]
+        name = input("Enter a name:\n")
+        ingredient = input("Enter ingredients:\n")
+        while ingredient:
+            ingredients.append(ingredient)
+            ingredient = input()
+        meal = input("Enter a meal type:\n")
+        time = input("Enter a preparation time:\n")
+        add_recipe(cookbook,name,ingredients,meal,time)
     elif opt == '2':
         delete_recipe(cookbook,input())
+    elif opt == '3':
+        print_details(cookbook,input("\nPlease enter a recipe name to get its details:\n"))
+    elif opt == '4':
+        print_name(cookbook)
     elif opt == '5':
         quit = False
+        print("\nCookbook closed. Goodbye !")
+        break
     else:
         print("Sorry, this option does not exist.")
     print_menu()

fa9ebd35aa38b812dae6dd416b22071f84ab2e81
dacortes <dacortes@student.42barcelona.com>
2023-03-14 09:09:47 +0100

    add ex04

---
+++ ex04/operations.py
@@ -0,0 +1,33 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    operations.py                                      :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/03/14 07:54:40 by dacortes          #+#    #+#              #
+#    Updated: 2023/03/14 09:06:58 by dacortes         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+
+if len(sys.argv) == 3:
+    num = sys.argv[1::]
+    if num[0].isdigit() == True and num[1].isdigit() == True:
+        a = int(num[0])
+        b = int(num[1])
+        print("Sum:       ",a + b)
+        print("Difference:",a - b)
+        print("Product:   ",a * b)
+        if b == 0:
+            print("Quotient:   ERROR (division by zero)")
+            print("Remainder:  ERROR (modulo by zero)")
+            exit
+        else :
+            print("Quotient:  ",a / b)
+            print("Remainder: ",a % b)
+    else :
+        print("AssertionError: only integers")
+elif len(sys.argv) >= 3:
+    print("AssertionError: too many arguments")

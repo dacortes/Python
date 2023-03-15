86ed4d320a176cad5a9a8970271aefd90558b0e6
dacortes <dacortes@student.42barcelona.com>
2023-03-15 19:52:43 +0100

    test

--- Module01/ex00/book.py
+++ Module01/ex00/book.py
@@ -6,7 +6,7 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/15 11:58:31 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/15 19:32:00 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/15 19:39:49 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -20,6 +20,7 @@ class Book:
             return (True)
         else:
             return (False)
+    #method to initialize the class
     def __init__(self, name):
         if self.check(name,'name') == False:
             print("Error")
@@ -28,7 +29,7 @@ class Book:
         self.last_update = datetime.now()
         self.creation_date = datetime.now()
         self.recipes_list = {"starter": [], "lunch": [], "dessert": []}
-
+    #method to get recipe by name
     def get_recipe_by_name(self, name):
         """Imprime la receta con el nombre \texttt{name} y devolver la instancia"""
         for recipes in self.recipes_list.values():
@@ -38,7 +39,7 @@ class Book:
                     return recipe
         print(f"No recipe found with name {name}")
         return (None)
-    
+    #method to get recipes by types
     def get_recipes_by_types(self, recipe_type):
         """Devuelve todas las recetas dado un recipe_type """
         if recipe_type not in self.recipes_list:
@@ -48,7 +49,7 @@ class Book:
         for i in recipes:
             print(i)
         return (recipes)
-    
+    #method to add recipe
     def add_recipe(self, recipe):
         if not isinstance(recipe, Recipe):
             print(f"Error: Expected an instance of class Recipe")
--- Module01/ex00/test.py
+++ Module01/ex00/test.py
@@ -6,21 +6,46 @@
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/15 14:56:30 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/15 16:31:01 by dacortes         ###   ########.fr        #
+#    Updated: 2023/03/15 19:40:05 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-#from book import Book
+import unittest
+from datetime import datetime
 from recipe import Recipe
+from book import Book
 
+class TestBookMethods(unittest.TestCase):
 
-'''=================================== TEST ============================================='''
-'''init_recipe = Recipe("bocadillo",2,10,['jamon', 'pan', 'queso', 'tomate'],'','comida')
-init_recipe = Recipe(1,2,10,['jamon', 'pan', 'queso', 'tomate'],'','comida')
-init_recipe = Recipe("bocadillo",'2',10,['jamon', 'pan', 'queso', 'tomate'],'','comida')
-tourte = Recipe("bocadillo",2,10,['jamon', 'pan', 'queso', 'tomate'],'','comida')
-to_print = str(tourte)
-print(to_print)'''
-tourte = Recipe("bocadillo",2,10,['jamon', 'pan', 'queso', 'tomate'],'','comida')
-to_print = str(tourte)
-print(to_print)
+    def setUp(self):
+        self.book = Book("Cookbook")
+
+        recipe1 = Recipe("Spaghetti Carbonara", 2, 30, ["pasta", "eggs", "bacon", "cheese"], "A classic Italian dish", "lunch")
+        recipe2 = Recipe("Tiramisu", 1, 45, ["coffee", "sugar", "eggs", "mascarpone", "ladyfingers"], "A traditional Italian dessert", "dessert")
+        recipe3 = Recipe("Caprese Salad", 1, 15, ["tomatoes", "mozzarella", "basil", "olive oil", "salt"], "A simple and fresh salad", "starter")
+
+        self.book.add_recipe(recipe1)
+        self.book.add_recipe(recipe2)
+        self.book.add_recipe(recipe3)
+
+    def test_get_recipe_by_name(self):
+        # Test for existing recipe
+        recipe = self.book.get_recipe_by_name("Tiramisu")
+        self.assertEqual(recipe.name, "Tiramisu")
+
+        # Test for non-existing recipe
+        recipe = self.book.get_recipe_by_name("Risotto")
+        self.assertIsNone(recipe)
+
+    def test_get_recipes_by_types(self):
+        # Test for existing type of recipe
+        recipes = self.book.get_recipes_by_types("lunch")
+        self.assertEqual(len(recipes), 1)
+        self.assertEqual(recipes[0].name, "Spaghetti Carbonara")
+
+        # Test for non-existing type of recipe
+        recipes = self.book.get_recipes_by_types("snack")
+        self.assertEqual(len(recipes), 0)
+
+if __name__ == '__main__':
+    unittest.main()

5ca16ba7b9c1db3a4b5005116d27c1ff1bdd8f54
dacortes <dacortes@student.42barcelona.com>
2023-03-22 18:51:39 +0100

    se agrega pruebas con otro analisis

--- TAPPX/prueba.py
+++ TAPPX/prueba.py
@@ -1,23 +1,24 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    main.py                                            :+:      :+:    :+:    #
+#    prueba.py                                          :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/03/21 15:00:20 by dacortes          #+#    #+#              #
-#    Updated: 2023/03/22 17:36:47 by dacortes         ###   ########.fr        #
+#    Created: 2023/03/22 17:26:26 by dacortes          #+#    #+#              #
+#    Updated: 2023/03/22 18:20:55 by dacortes         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 '''.................................. libraries .............................'''
 
-from termcolor import colored
-from tqdm import tqdm
-import pandas as pd
-import json
+from termcolor import colored #print colors
+from tqdm import tqdm #bar
+import pandas as pd #table json
+import json #open close
+import spacy #
 import numpy as np
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer #analyze
 
 '''................................ Funtions ................................'''
 
@@ -27,14 +28,8 @@ def read_json(filename):
     with open(filename, 'r') as f:
         data = json.load(f)
     return  (data)
-###...............................  Push_dic ................................###
 
-'''def push_dic_dic(dic, id_article, id_video, score):
-    if id_article not in dic:
-        dic[id_article] = {}
-    if id_video not in dic[id_article]:
-        dic[id_article][id_video] = {}
-    dic[id_article][id_video]['score'] = score  '''
+###...............................  Push_dic ................................###
 
 def push_dic_dic(dic, id_article, id_video, score):
     if id_article not in dic:
@@ -46,8 +41,22 @@ def push_dic_dic(dic, id_article, id_video, score):
 
 def push_dic(dic, id_article):
     dic[id_article] = {}
+###...............................  extract_keyw ............................###
 
-###...............................  Analyze_keys ............................###
+def search_keywords(text):
+    article = text
+    article = text.lower()
+    doc = nlp(article)
+    num_keywords = 20
+    keywords = []
+    for token in doc:
+        if not token.is_stop and token.pos_ in ['NOUN', 'PROPN', 'ADJ', 'PRON']:
+            keywords.append(token.lemma_)
+            if len (keywords) == num_keywords:
+                break
+    return (keywords)
+
+###...............................  Analyze_keyw ............................###
 
 #keywords with keywords
 def score_key(df, article):
@@ -91,14 +100,14 @@ def analyze_keywords(df_video):
         pbar.update(1)
     pbar.close()
     print(colored("\t\t\tAnalisis finalizado","green"))
+    
+###...............................  Analyze_text ............................###
 
-###...............................  Analyze_tittle ..........................###
-
-def score_tittle(df, article):
+def score_text(df, article):
     scores = []
     i = 0
     while i < len(in_video):
-        video = ' '.join(df['keywords'][i])
+        video = ' '.join(search_keywords(df['text'][i]))
         m_video = model.encode(video)
         m_article = model.encode(article)
         res = np.dot(m_article, m_video)/(np.linalg.norm(m_article)*np.linalg.norm(m_video))
@@ -107,16 +116,16 @@ def score_tittle(df, article):
     scores.sort(reverse=True)
     return (scores)
 
-def analyze_title(df_video):
+def analyze_text(df_video):
     i = 0
     num = 1
-    tittle = colored("Analisis por Text con kywords", attrs=["bold"])
+    tittle = colored("Analisis por Text con Text", attrs=["bold"])
     #print("............ "+ colored(tittle, "blue") + " ............")
     print(colored(f"\n\t\t\t{tittle}", "blue"))
     pbar = tqdm(total=len(in_article))
     while i < len(in_article):
-        article = df_article['text'][in_article[i]]
-        scores = score_tittle(df_video, article)
+        article = ' '.join(search_keywords(df_article['text'][in_article[i]]))
+        scores = score_text(df_video, article)
         #print(colored("Numero de articulo:","blue") + f" {num}\nid: {in_article[i]}")
         v1 = round((scores[0][0] * 10), 1)
         id1 = scores[0][1]
@@ -150,6 +159,7 @@ def analyze_score(socore, video):
 '''.................................. Vars ..................................'''
 
 model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v1')
+nlp = spacy.load('es_core_news_md')
 
 '''.................................. Main ..................................'''
 
@@ -168,7 +178,7 @@ if __name__ == "__main__":
     print(colored(f"videos con los que se pueden relacionar:", "blue") +
           f" {df_video.shape[0]}")
 ###............................... Score ....................................###
-    analyze_keywords(df_video)
-    analyze_title(df_video)
-    with open('data.json', 'w') as file:
+    #analyze_keywords(df_video)
+    analyze_text(df_video)
+    with open('data1.json', 'w') as file:
         json.dump(dic_outut, file, indent=2)
